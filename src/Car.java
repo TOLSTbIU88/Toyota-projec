410f5bc670aee7cfc1aa074a59fe5ecd32d84bdc
konstantinvolkov <titanin88@gmail.com>
2022-07-04 14:51:54 +0300

    создал нов репоз

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,22 @@
 public class Car {
 
+    public static Camry NovCamry (){
+        Camry camry = new Camry();
+        return camry;
+    }
+
+    public static Solara NovSolara (){
+        Solara solara = new Solara();
+        return solara;
+    }
+
+    public static Hiance NovHiance (){
+        Hiance hiance = new Hiance();
+        return hiance;
+    }
+
+    public static Dyna NovDyna (){
+        Dyna dyna = new Dyna();
+        return dyna;
+    }
 }
