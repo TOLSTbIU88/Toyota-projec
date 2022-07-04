c675b4c6c4ea5544cd8dca2b4efbb67e78fbaa7a
konstantinvolkov <titanin88@gmail.com>
2022-07-04 17:20:17 +0300

    создал нов репоз

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,34 @@
 public class Car {
 
+    private int countGasTanks;
+    private boolean engine;
+
+    private boolean electrics;
+
+    public Car(boolean engine) {
+        this.engine = engine;
+    }
+
+    public boolean isEngine() {
+        return engine;
+    }
+
+    public void setEngine(boolean engine) {
+        this.engine = engine;
+    }
+
+    public Car(int countGasTanks) {
+        this.countGasTanks = countGasTanks;
+    }
+
+    public int getCountGasTanks() {
+        return countGasTanks;
+    }
+
+    public void setCountGasTanks(int countGasTanks) {
+        this.countGasTanks = countGasTanks;
+    }
+
     public static Camry NovCamry (){
         Camry camry = new Camry();
         return camry;
