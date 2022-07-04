44cfbbd52cc81c57324d9b83d0dc3fd426ce18dd
konstantinvolkov <titanin88@gmail.com>
2022-07-04 17:23:21 +0300

    создал нов репоз

--- src/Car.java
+++ src/Car.java
@@ -5,6 +5,44 @@ public class Car {
     private boolean electrics;
     private boolean headlights;
 
+    public Car(int countGasTanks, boolean engine, boolean electrics, boolean headlights) {
+        this.countGasTanks = countGasTanks;
+        this.engine = engine;
+        this.electrics = electrics;
+        this.headlights = headlights;
+    }
+
+    public int getCountGasTanks() {
+        return countGasTanks;
+    }
+
+    public void setCountGasTanks(int countGasTanks) {
+        this.countGasTanks = countGasTanks;
+    }
+
+    public boolean isEngine() {
+        return engine;
+    }
+
+    public void setEngine(boolean engine) {
+        this.engine = engine;
+    }
+
+    public boolean isElectrics() {
+        return electrics;
+    }
+
+    public void setElectrics(boolean electrics) {
+        this.electrics = electrics;
+    }
+
+    public boolean isHeadlights() {
+        return headlights;
+    }
+
+    public void setHeadlights(boolean headlights) {
+        this.headlights = headlights;
+    }
 
     public static Camry NovCamry (){
         Camry camry = new Camry();
