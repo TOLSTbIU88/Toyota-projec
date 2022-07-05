2f37672e9b74a303b7b7702697a55bf3dc8860be
konstantinvolkov <titanin88@gmail.com>
2022-07-05 14:10:20 +0300

    тесты....

--- src/auto/detail/Detail.java
+++ src/auto/detail/Detail.java
@@ -1,4 +1,10 @@
 package auto.detail;
 
 public abstract class Detail {
+
+    private boolean isWorkable;
+
+    public Detail(boolean isWorkable) {
+        this.isWorkable = isWorkable;
+    }
 }
