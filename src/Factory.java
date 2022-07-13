556e431f8c3419e9cb41bd04483515acab7cc757
konstantinvolkov <titanin88@gmail.com>
2022-07-14 01:21:18 +0300

    тайота ....

--- src/Factory.java
+++ src/Factory.java
@@ -1,11 +1,31 @@
-import auto.typecar.carsobj.detail.Wheel;
-import auto.typecar.carsobj.detail.WheelRadius;
+import auto.typecar.carsobj.detail.*;
 
 public class Factory {
+
+    private Country country;
+
+    public Electric createElectric(){
+        return new Electric(true);
+    }
+
+    public Engine createEngine(){
+        return  new Engine( true);
+    }
+
+    public GasTank createGasTank() {
+        return new GasTank(0);
+    }
+
+    public Headlight createHeadlight(){
+        return new Headlight(true);
+    }
+
+
+
     public static Wheel[] createWheelsWithRadius(WheelRadius wheelRadius) {
         Wheel[] wheels = new Wheel[4];
         for (int i = 0; i < wheels.length; i++) {
-            wheels[i] = new Wheel(false, wheelRadius);
+            wheels[i] = new Wheel(true, wheelRadius);
         }
         return wheels;
     }
