94b8d8a9407d5a88f655c283310c9e3e470a6e10
konstantinvolkov <titanin88@gmail.com>
2022-07-21 19:21:45 +0300

    тайота ....

--- src/LineCar.java
+++ src/LineCar.java
@@ -1,4 +1,7 @@
 import auto.typecar.carsobj.Camry;
+import auto.typecar.carsobj.Dyna;
+import auto.typecar.carsobj.Hiance;
+import auto.typecar.carsobj.Solara;
 import auto.typecar.carsobj.detail.*;
 
 public class LineCar {
@@ -16,17 +19,52 @@ public class LineCar {
     }
 
     public Camry createCamry(String color, float price) {
-        Electric electric = Factory.createElectric();
-        Engine engine = Factory.createEngine();
-        GasTank gasTank = Factory.createGasTank();
-        Headlight headLights = Factory.createHeadlight();
+        Electric electric = factory.createElectric();
+        Engine engine = factory.createEngine();
+        GasTank gasTank = factory.createGasTank();
+        Headlight headLights = factory.createHeadlight();
 
-        Wheel[] wheels = Factory.createWheels(WheelRadius.R17);
-        Usb usb = Factory.createUsb();
+        Wheel[] wheels = factory.createWheels(WheelRadius.R17);
+        Usb usb = factory.createUsb();
 
-        return new Camry(color, 250, Transmission.AUTO, false, price, gasTank, engine,
-                electric, headLights, wheels, false, usb);
+        return new Camry(20000,200,"red",Transmission.AUTO,
+                true,wheels,gasTank,engine,electric,headLights,true,usb);
     }
 
+    public Solara createSolara(String color, float price) {
+        Electric electric = factory.createElectric();
+        Engine engine = factory.createEngine();
+        GasTank gasTank = factory.createGasTank();
+        Headlight headLights = factory.createHeadlight();
+
+        Wheel[] wheels = factory.createWheels(WheelRadius.R16);
+        MiniFrigde miniFrigde = factory.createMiniFridge();
+
+        return new Solara(25000,150,"red",Transmission.ROBOT,
+                true,wheels,gasTank,engine,electric,headLights,true,miniFrigde);
+    }
+    public Dyna createDyna(String color, float price) {
+        Electric electric = factory.createElectric();
+        Engine engine = factory.createEngine();
+        GasTank gasTank = factory.createGasTank();
+        Headlight headLights = factory.createHeadlight();
+
+        Wheel[] wheels = factory.createWheels(WheelRadius.R20);
+        Socket socket = factory.createSocket();
+
+        return new Dyna(35000,60,"red",Transmission.MANUAL,
+                true,wheels,gasTank,engine,electric,headLights,5000,socket);
+    }
+    public Hiance createHiance(String color, float price) {
+        Electric electric = factory.createElectric();
+        Engine engine = factory.createEngine();
+        GasTank gasTank = factory.createGasTank();
+        Headlight headLights = factory.createHeadlight();
+
+        Wheel[] wheels = factory.createWheels(WheelRadius.R20);
+        Wheel safeWheel = factory.createSafeWheel(WheelRadius.R20);
+
+        return new Hiance(35000,60,"red",Transmission.MANUAL,
+                true,wheels,gasTank,engine,electric,headLights,5000,safeWheel);
 
 }
